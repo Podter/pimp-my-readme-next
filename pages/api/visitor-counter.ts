811868c61554c16104bffa30615572494e17d029
Podter <48936015+Podter@users.noreply.github.com>
2022-10-05 20:57:44 +0700

    Create visitor-counter.ts

---
+++ pages/api/visitor-counter.ts
@@ -0,0 +1,18 @@
+import { getVisitorCounter } from "../../util/svg";
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // Get Query params
+  const page = req.query.page;
+  const response = await axios.get(
+    `https://api.countapi.xyz/hit/webappio/${page}`
+  );
+  const data = response.data || 1;
+  const svg = getVisitorCounter(data.value);
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.send(svg);
+}
