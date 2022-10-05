4a70e867ce931fccf13592aa976959ecc350aa51
Podter <48936015+Podter@users.noreply.github.com>
2022-10-05 22:57:46 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Link from "next/link";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("https://github.com/Podter/pimp-my-readme-next");
+  });
+
   return (
     <div>
-      <h1>Hello world!</h1>
+      <p>Redirecting you to GitHub</p>
+      <Link href={"https://github.com/Podter/pimp-my-readme-next"}>
+        <a>Podter/pimp-my-readme-next</a>
+      </Link>
     </div>
   );
 }
